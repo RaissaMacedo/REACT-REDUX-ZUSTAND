bf73ff6111bff6fdc4f71d46f7416899f919af7b
RaissaMacedo <raissamacedo7@gmail.com>
2024-07-09 18:08:08 -0300

    feat: aplication with redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook } from 'react-redux'
+import { player } from "./slices/player";
 
 // state global
 export const store = configureStore({
   // information to share in between components
   reducer: {
+    player,
   }
 })
 
